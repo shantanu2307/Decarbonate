060593424f042ec9b4c5e8725167f5ae4947d847
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2021-11-16 12:58:34 +0530

    Flask init

--- app/main.py
+++ app/main.py
@@ -1,39 +1,66 @@
-from keras.models import model_from_json
-from keras.models import load_model
-import numpy as np
-import json
-import tensorflow as tf
-from PIL import Image
-
 import matplotlib.pyplot as plt
+from PIL import Image
+import tensorflow as tf
+import sys
+import json
+import numpy as np
+from keras.models import load_model
+from keras.models import model_from_json
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
 
-def get_img_arr():
-    # data = json.loads(sys.argv[1])
-    # data = json.loads(input())
-    filename = "/Users/shantanugoyal/Desktop/Decarbonate-2/app/glass166.jpg"
-    image=Image.open(filename)
-    width, height = image.size
+
+def rgba2rgb(rgba, background=(255, 255, 255)):
+    row, col, ch = rgba.shape
+
+    rgb = np.zeros((row, col, 3), dtype='float32')
+    r, g, b, a = rgba[:, :, 0], rgba[:, :, 1], rgba[:, :, 2], rgba[:, :, 3]
+    a = np.asarray(a, dtype='float32') / 255.0
+
+    return np.asarray(rgb, dtype='float32')
+
+
+def get_img_arr(image, height, width):
+    image = list(map(int, image))
     image = np.array(image)
-    im = tf.image.resize_with_crop_or_pad(image, 300, 300)
-    im = np.array(im)
-    return im
+    image = np.reshape(image, (height, width, 4))
+    image = rgba2rgb(image)/255.0
+    image = tf.image.resize_with_crop_or_pad(image, 300, 300)
+    image = np.array(image)
+    return image
 
 
-def main():
-    x = get_img_arr()
+def main(image,height, width):
+    x = get_img_arr(image,height, width)
     x = np.reshape(x, (1, 300, 300, 3))
-    json_file=open('app/model.json', 'r') 
+    json_file = open('app/model.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
     loaded_model.load_weights("app/model.h5")
     print("Loaded model from disk")
     preds = loaded_model.predict(x)
-    print(type(preds))
+    objects = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
+    preds = np.squeeze(preds)
+    ans = 0
+    for i in range(len(preds)):
+        preds[i] *= i
+        ans += int(preds[i])
+
+    print(objects[ans])
+    return objects[ans]
 
 
-if __name__ == '__main__':
-	try :
-		main()
-	except Exception as e :
-		print('error', str(e))
+@app.route("/", methods=["GET", "POST"])
+def getPrice():
+    print("Hi")
+    query = request.get_json()
+    # print(query)
+    image=query['img']
+    ans = main(image,query['height'],query['width'])
+    List = []
+    List.append(ans)
+    return json.dumps(List)
--- src/components/emissions/PictureEm.js
+++ src/components/emissions/PictureEm.js
@@ -1,9 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuth } from './../../firebase/auth';
 import axios from 'axios';
 
 export default function PictureEm() {
-    
+
     const [src, setSrc] = useState('');
     const [waste, setWaste] = useState('');
     const [carbon, setCarbon] = useState(0);
@@ -11,7 +11,7 @@ export default function PictureEm() {
     function imgUpload(event) {
         try {
             setSrc(URL.createObjectURL(event.target.files[0]));
-        } catch(err) {
+        } catch (err) {
             console.log(err);
         }
         try {
@@ -20,7 +20,7 @@ export default function PictureEm() {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             var img = new Image();
 
-            img.onload = function() {
+            img.onload = function () {
                 var height = img.height;
                 var width = img.width;
                 canvas.height = height;
@@ -29,31 +29,28 @@ export default function PictureEm() {
             }
 
             img.src = URL.createObjectURL(event.target.files[0]);
-            
-        } catch(err) {
+
+        } catch (err) {
             console.log(err);
         }
-        
+
     }
 
-    function calcEm() {
-        var canvas = document.getElementById('canvas');
-        var ctx = canvas.getContext('2d');
-        var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+    async function calcEm() {
+        var canvas = await document.getElementById('canvas');
+        var ctx = await canvas.getContext('2d');
+        var imgData = await ctx.getImageData(0, 0, canvas.width, canvas.height).data;
         console.log(imgData);
-        let data = {img: imgData, height: canvas.height, width: canvas.width};
+        let data = { img: imgData, height: canvas.height, width: canvas.width };
         console.log(data);
-        axios.post('http://localhost:8080/img', data)
-            .then(res => {
-                console.log(res.data.data);
-                setCarbon(res.data.data.carbon);
-                setWaste(res.data.data.item);
-            })
-            .catch(err => console.log(err));
+        const res = await axios.post('http://localhost:5000/', data);
+        console.log(res.data[0]);
+        // setCarbon(res.data.data.carbon);
+        setWaste(res.data[0]);
     }
 
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    
+
     const { currentUser } = useAuth();
 
     useEffect(() => {
@@ -80,16 +77,16 @@ export default function PictureEm() {
                     <tr>
                         <td>
                             <form>
-                                <input type="file"  accept="image/*" name="image" id="file" onInput={imgUpload} />
-                                <br/>
+                                <input type="file" accept="image/*" name="image" id="file" onInput={imgUpload} />
+                                <br />
                                 <p>{src}</p>
-                                <br/>
+                                <br />
                                 <canvas id='canvas' height={0} width={0}></canvas>
-                                <br/>
-                                <button type="button" onClick={calcEm} className='submit-btn'>Find Emission</button>              
+                                <br />
+                                <button type="button" onClick={calcEm} className='submit-btn'>Find Emission</button>
                             </form>
                         </td>
-                        <td style={{width:'30%',}}>
+                        <td style={{ width: '30%', }}>
                             <h4>Your emissions</h4>
                             <p>Object: {waste}</p>
                             <h6>Carbon emission: {carbon} kgs</h6>
